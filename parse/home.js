cdb09ce6f7dd92aa763d1fa55763fb3e8475860d
josher93 <josher93@gmail.com>
2015-11-09 16:36:59 -0600

    Se añadió función que revisa si ya existe Date para examen

--- parse/home.js
+++ parse/home.js
@@ -1,4 +1,7 @@
 // JavaScript Document
+
+var existsTime = false;
+
 function save() {
     var Programmation = Parse.Object.extend("Programmation");
     var programmation = new Programmation();
@@ -8,27 +11,51 @@ function save() {
     programmation.set("IdExam", document.getElementById("ddlSubject").value);
     programmation.set("Date", document.getElementById("txtDate").value + " " + document.getElementById("txtTime").value);
     */
-    programmation.save({
-        IdGroup: parseInt(document.getElementById("ddlGroup").value),
-        IdLocation:parseInt(document.getElementById("ddlLocation").value),
-        IdExam: parseInt(document.getElementById("ddlSubject").value),
-        Date: new Date(document.getElementById("txtDate").value + " " + document.getElementById("txtTime").value)
-    }, {
-        success: function (programmation) {
-            // Execute any logic that should take place after the object is saved.
-            //alert('New object created with objectId: ' + programmation.id);
-        },
-        error: function (programmation, error) {
-            // Execute any logic that should take place if the save fails.
-            // error is a Parse.Error with an error code and message.
-            //alert('Failed to create new object, with error code: ' + error.message);
-        }
-    });
+
+    validateTime();
+
+    if (existsTime == true) {
+        alert("Error: You cannto set same time for an exam");
+    }
+    else {
+        programmation.save({
+            IdGroup: parseInt(document.getElementById("ddlGroup").value),
+            IdLocation: parseInt(document.getElementById("ddlLocation").value),
+            IdExam: parseInt(document.getElementById("ddlSubject").value),
+            Date: new Date(document.getElementById("txtDate").value + " " + document.getElementById("txtTime").value)
+        }, {
+            success: function (programmation) {
+                // Execute any logic that should take place after the object is saved.
+                //alert('New object created with objectId: ' + programmation.id);
+            },
+            error: function (programmation, error) {
+                // Execute any logic that should take place if the save fails.
+                // error is a Parse.Error with an error code and message.
+                //alert('Failed to create new object, with error code: ' + error.message);
+            }
+        });
+    }
 }
-function alertar()
-{
+function alertar() {
     alert(document.getElementById("ddlGroup").value);
     alert(document.getElementById("ddlLocation").value);
     alert(document.getElementById("ddlSubject").value);
     alert(document.getElementById("txtDate").value + " " + document.getElementById("txtTime").value)
 }
+
+function validateTime() {
+    var existingTime = Parse.Object.extend("Programmation");
+    var queryTime = new Parse.Query(existingTime);
+    var examDate = new Date(document.getElementById("txtDate").value + " " + document.getElementById("txtTime").value)
+    query.equalTo("Date", examDate);
+    query.first({
+        success: function (object) {
+            // Successfully retrieved the object.
+            existsTime = true;
+        },
+        error: function (error) {
+            alert("Error: " + error.code + " " + error.message);
+        }
+    });
+}
+
